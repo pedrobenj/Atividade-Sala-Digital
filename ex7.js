c60a0bfee6b08ebcb87ca460bf813adde35595d0
Pedro Benjamin <benjamin23ph@gmail.com>
2023-10-20 19:29:43 -0300

    commit-ajuste3

---
+++ ex7.js
@@ -0,0 +1,10 @@
+let a = 2
+let soma = 0
+
+if (a % 2 === 0) {
+    soma= a += 5
+    console.log("Soma é: " + soma)
+} else {
+    soma = a+= 8
+    console.log("Soma é: " + soma)
+}
---
+++ ex8.js
@@ -0,0 +1,7 @@
+let a = 2
+let b = 4
+let c = 6
+
+if (a<b<c) {
+    console.log(c, b, a)
+}
