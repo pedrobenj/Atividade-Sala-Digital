c6feed8f73c78fb8c843364f5e0a5c5061891635
Pedro Benjamin <benjamin23ph@gmail.com>
2023-10-20 19:04:31 -0300

    commit-ajuste

---
+++ ex5.js
@@ -0,0 +1,10 @@
+let numb = 10
+let resultado = 0
+
+if (numb > 0) {
+    resultado = numb *= 2
+    console.log("Resultado é: " + resultado)
+} else {
+    resultado = numb *= 3
+    console.log("Resultado é: " + resultado)
+}
